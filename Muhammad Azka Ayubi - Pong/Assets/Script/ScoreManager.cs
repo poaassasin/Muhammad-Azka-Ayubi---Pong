fc26aaa12e6b5fee8209f6fd1188c13b5966d913
poaassasin <azkaujicoba@gmail.com>
2022-11-06 00:02:56 +0700

    Adding UI & Game Manager

---
+++ Muhammad Azka Ayubi - Pong/Assets/Script/ScoreManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int rightScore; public int leftScore; public int maxScore;
+    public BallController ball;
+    public void AddRightScore(int increment)
+    {
+
+        rightScore = rightScore+increment;
+        ball.ResetBall();
+
+        if (rightScore >= maxScore)
+        {
+            GameOver();
+        }
+    }
+    public void AddLeftScore(int increment)
+    {
+        leftScore = leftScore+increment;
+
+        if (leftScore >= maxScore)
+        {
+            GameOver();
+        }
+    }
+    public void GameOver()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
